354f3569208aa15c6726ccfc62d0408a77cf2a50
Dan Ngo <danngo.au@gmail.com>
2025-04-10 15:56:42 +1000

    PA-113 minor fix

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from . import *
 
 router = DefaultRouter(trailing_slash="")  # No trailing slash
-router.register(r"user", UserViewSet, basename="user")
+router.register(r"user", UserViewSet, "user")
 
 urlpatterns = router.urls
