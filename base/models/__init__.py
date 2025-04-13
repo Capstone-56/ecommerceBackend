32a0be9bbc75809811294ce02fd74c8c3099051d
HaiderMaan <abdulrehmanmhaider@gmail.com>
2025-04-13 15:03:54 +1000

    __init__.py

--- base/models/__init__.py
+++ base/models/__init__.py
@@ -1,2 +1,6 @@
 from .user_model import UserModel
 from .product_model import ProductModel
+from .category_model import CategoryModel
+from .product_category_model import ProductCategoryModel
+from .product_size_model import ProductSizeModel
+
