874ad6321e58403f8979c81ff4532ec68ed02632
XMcPhee <103607491@student.swin.edu.au>
2025-04-13 11:42:43 +1000

    Initial product table and serving of api

--- api/serializers.py
+++ api/serializers.py
@@ -5,3 +5,12 @@ class UserModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserModel
         fields = ["username", "name", "email", "password"]
+
+class ProductModelSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the ProductModel.
+    Converts ProductModel instances to and from JSON format for API interactions.
+    """
+    class Meta:
+        model = ProductModel
+        fields = ["NAME", "DESCRIPTION", "IMAGES"]
--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
+
 from . import *
 
 router = DefaultRouter(trailing_slash="")  # No trailing slash
 router.register(r"user", UserViewSet, "user")
+router.register(r"product", ProductViewSet, "product")
 
 urlpatterns = router.urls
