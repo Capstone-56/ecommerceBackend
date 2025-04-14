bfa91592e047216e1ff6807523b03deb409ac802
Dan Ngo <danngo.au@gmail.com>
2025-04-14 21:14:16 +1000

    PA-130 added boolean `featured` property to product model

--- base/models/product_model.py
+++ base/models/product_model.py
@@ -10,11 +10,13 @@ class ProductModel(models.Model):
         name        Name of the product to sell.
         description The associated description of what the product is.
         images      Image urls to display on frontend.
+         featured    Boolean flag indicating whether the product is featured.
     """
     id          = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
     name        = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     images      = ArrayField(models.CharField(max_length=1000), blank=True)
+    featured    = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
