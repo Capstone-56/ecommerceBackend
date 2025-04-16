b9b037614124969a7df8e938a367593e9695ef12
Dan Ngo <danngo.au@gmail.com>
2025-04-16 19:20:12 +1000

    PA-133 Category entity, controller and seed data

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ from . import *
 router = DefaultRouter(trailing_slash="")  # No trailing slash
 router.register(r"user", UserViewSet, "user")
 router.register(r"product", ProductViewSet, "product")
+router.register(r"category", CategoryViewSet, "category")
 
 urlpatterns = router.urls
