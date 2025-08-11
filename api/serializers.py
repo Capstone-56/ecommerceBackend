03298816f99ce8c07264793520de465417d74052
XMcPhee <103607491@student.swin.edu.au>
2025-08-11 15:28:45 +1000

    PA-222: Fix

--- api/serializers.py
+++ api/serializers.py
@@ -63,7 +63,7 @@ class ProductItemModelSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ProductItemModel
-        fields = fields = ["id", "sku", "stock", "price", "imageUrls", "variations"]
+        fields = fields = ["id", "product", "sku", "stock", "price", "imageUrls", "variations"]
     
 class ProductModelSerializer(serializers.ModelSerializer):
     price = serializers.SerializerMethodField()
