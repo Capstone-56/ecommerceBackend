fe14822099fd43340e7e2aecb2805f2f0706ec2e
OliverBraySwin <103070831@student.swin.edu.au>
2025-08-19 16:15:54 +1000

    Renaming again

--- api/controllers/__init__.py
+++ api/controllers/__init__.py
@@ -5,5 +5,5 @@ from .address_view import AddressViewSet
 from .shopping_cart_view import ShoppingCartViewSet
 from .product_item_view import ProductItemViewSet
 from .stripe_view import StripeViewSet
-from .order_status_view import OrdersViewSet
+from .order_status_view import OrderStatusViewSet
 from .location_view import LocationViewSet
