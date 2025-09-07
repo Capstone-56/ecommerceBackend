61fda5da48b1cbd1e9103bd9375afdd2ac7ba186
Dan Ngo <danngo.au@gmail.com>
2025-09-07 22:19:54 +1000

    Update buildspec.yml & settings.py

--- ecommerceBackend/settings.py
+++ ecommerceBackend/settings.py
@@ -140,6 +140,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
